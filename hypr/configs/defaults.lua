aa8228b0459b84db035c9acbb1d8e402cc3ad704
Carmelo Rositano <carbarooster@gmail.com>
2026-08-17 09:27:00 +0930

    Removes ignored files and applies allowlist

--- hypr/configs/defaults.lua
+++
@@ -1,8 +0,0 @@
-local alias = {
-    terminal    = "kitty",
-    fileManager = "nautilus",
-    menu        = "wofi --show drun",
-    browser     = "flatpak run app.zen_browser.zen",
-}
-
-return alias
--- waybar/check-updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-n=$(checkupdates 2>/dev/null | wc -l)
-
-if [ "$n" -gt 0 ]; then
-    printf '{"text":"●","class":"updates"}\n'
-else
-    printf '{"text":"","class":"no-updates"}\n'
-fi
